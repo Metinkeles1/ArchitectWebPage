0f05dd45566e8a61b643bb426b8b63afd24675a4
Metin Keleş <metinkeles677@gmail.com>
2023-05-08 14:28:47 +0300

    CreateManager

---
+++ BusinessLayer/Concrete/ProjectManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProjectManager : IProjectService
+    {
+        IProjectDal _projectDal;
+
+        public ProjectManager(IProjectDal projectDal)
+        {
+            _projectDal = projectDal;
+        }
+
+        public void TAdd(Project t)
+        {
+            _projectDal.Insert(t);
+        }
+
+        public void TDelete(Project t)
+        {
+            _projectDal.Delete(t);
+        }
+
+        public Project TGetByID(int id)
+        {
+            return _projectDal.GetByID(id); 
+        }
+
+        public List<Project> TGetList()
+        {
+            return _projectDal.GetAll();
+        }
+
+        public void TUpdate(Project t)
+        {
+            _projectDal.Update(t);
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/ServiceItemManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ServiceItemManager : IServiceItemService
+    {
+        IServiceItemDal _serviceItemDal;
+
+        public ServiceItemManager(IServiceItemDal serviceItemDal)
+        {
+            _serviceItemDal = serviceItemDal;
+        }
+
+        public void TAdd(ServiceItem t)
+        {
+            _serviceItemDal.Insert(t);
+        }
+
+        public void TDelete(ServiceItem t)
+        {
+            _serviceItemDal.Delete(t);
+        }
+
+        public ServiceItem TGetByID(int id)
+        {
+            return _serviceItemDal.GetByID(id);
+        }
+
+        public List<ServiceItem> TGetList()
+        {
+            return _serviceItemDal.GetAll();
+        }
+
+        public void TUpdate(ServiceItem t)
+        {
+            _serviceItemDal.Update(t);
+        }
+    }
+}
