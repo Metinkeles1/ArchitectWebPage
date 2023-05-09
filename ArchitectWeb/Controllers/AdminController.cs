cebf8993624e960848538ae362f79d690184cc18
Metin Keleş <metinkeles677@gmail.com>
2023-05-09 11:51:47 +0300

    AdminController&AdminLayout

---
+++ ArchitectWeb/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArchitectWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
