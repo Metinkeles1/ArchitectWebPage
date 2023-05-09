0a78a70db4af3cdc2314a67286cebfe04cfd822a
Metin Keleş <metinkeles677@gmail.com>
2023-05-09 11:51:34 +0300

    FeatureController&PartialSideBar

---
+++ ArchitectWeb/Controllers/FeatureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArchitectWeb.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
