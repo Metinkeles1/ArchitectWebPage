1123b8aeefaa897bf723df71bd6209992e67de42
Metin Keleş <metinkeles677@gmail.com>
2023-05-12 21:45:59 +0300

    RuleForProject

---
+++ BusinessLayer/ValidationRules/ProjectValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ProjectValidator : AbstractValidator<Project>
+    {
+        public ProjectValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("project name cannot be empty");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("ImageUrl cannot be empty");
+            RuleFor(x => x.ImageUrlSmall).NotEmpty().WithMessage("Small Image Url cannot be empty");
+            RuleFor(x => x.ProjectUrl).NotEmpty().WithMessage("ImageUrl cannot be empty");
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("Project name must be at least 5 characters");
+            
+        }
+    }
+}
