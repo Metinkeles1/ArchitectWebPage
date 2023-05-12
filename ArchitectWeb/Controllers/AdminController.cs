6abb9cedefaee6f7caeee461b4d1434244c92825
Metin Keleş <metinkeles677@gmail.com>
2023-05-12 21:19:28 +0300

    AddProject&ListProjectViewandController

--- ArchitectWeb/Controllers/AdminController.cs
+++ ArchitectWeb/Controllers/AdminController.cs
@@ -8,5 +8,29 @@ namespace ArchitectWeb.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavigationPartial()
+        {
+            return PartialView();
+        }
     }
 }
---
+++ ArchitectWeb/Controllers/ProjectController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArchitectWeb.Controllers
+{
+    public class ProjectController : Controller
+    {
+        ProjectManager projectManager = new ProjectManager(new EfProjectDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Project List";
+            ViewBag.v2 = "Projects";
+            ViewBag.v3 = "Project List";
+            var values = projectManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddProject()
+        {
+            ViewBag.v1 = "Project List";
+            ViewBag.v2 = "Projects";
+            ViewBag.v3 = "Add Project";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProject(Project p)
+        {           
+            projectManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
