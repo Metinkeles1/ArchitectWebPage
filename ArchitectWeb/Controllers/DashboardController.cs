c664da787a8d561b5ea943d1541b956237fd1b6f
Metin Keleş <metinkeles677@gmail.com>
2023-05-13 14:45:23 +0300

    ImportNewThema

---
+++ ArchitectWeb/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArchitectWeb.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Dashboard";
+            ViewBag.v2 = "Dashboard";
+            ViewBag.v3 = "Dashboard Page";
+            return View();
+        }
+    }
+}
---
+++ ArchitectWeb/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArchitectWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
