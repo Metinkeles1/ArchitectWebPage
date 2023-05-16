868f6d70fc6d2d7509574dd0dea9ba167555f66b
Metin Keleş <metinkeles677@gmail.com>
2023-05-16 09:24:49 +0300

    FeatureStatisticPartialView

--- ArchitectWeb/ViewComponents/Dashboard/FeatureStatistics.cs
+++ ArchitectWeb/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ArchitectWeb.ViewComponents.Dashboard
 {
     public class FeatureStatistics : ViewComponent
     {
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.v1 = c.Projects.Count();
+            ViewBag.v2 = c.Messages.Where(x => x.Status == false).Count();
+            ViewBag.v3 = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.v4 = c.SocialMedias.Count();
             return View();
         }
     }
