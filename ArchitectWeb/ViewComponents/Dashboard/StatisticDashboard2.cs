a6b769c37c0579867687e78fe06b3e2c297b253f
Metin Keleş <metinkeles677@gmail.com>
2023-05-16 11:50:20 +0300

    DashboardStatistic2

---
+++ ArchitectWeb/ViewComponents/Dashboard/StatisticDashboard2.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace ArchitectWeb.ViewComponents.Dashboard
+{
+    public class StatisticDashboard2 : ViewComponent
+    {
+        Context c = new Context();
+        public ViewViewComponentResult Invoke() 
+        {
+            ViewBag.v1 = c.Messages.Count();
+            ViewBag.v2 = c.Projects.Count();
+            ViewBag.v3 = c.Services.Count();
+            return View(); 
+        }
+    }
+}
