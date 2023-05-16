4ebc5e5ec4c1c38965883d148b10e09a8b73b1e0
Metin Keleş <metinkeles677@gmail.com>
2023-05-16 09:16:36 +0300

    NewAdminLayoutDashboard

---
+++ ArchitectWeb/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArchitectWeb.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
